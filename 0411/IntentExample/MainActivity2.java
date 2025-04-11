a2de43ac44bdb5f4fabfca68972e96d2e1c8e7ab
heoheoyun <105352128+heoheoyun@users.noreply.github.com>
2025-04-11 14:53:32 +0900

    Add files via upload

---
+++ 0411/IntentExample/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.example.intentexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        Button b = (Button) findViewById(R.id.Button01);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
